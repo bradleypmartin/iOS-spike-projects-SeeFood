efdc2acce0127e215e7656c210812bf32d59526e
bradleypmartin <brad.martin@ibotta.com>
2019-02-26 22:09:41 -0700

    finishing up with hotdog identifier for now :)

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -24,17 +24,54 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // may want to set this to true in a more detailed app
         imagePicker.allowsEditing = false
         
-        // once user has selected an image, want to call delegate method
-
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let userPickedImage = info[.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            // converting to CIImage that allows Vision framework/ML interaction
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert user's UIImage to CIImage.")
+            }
+            
+            // once user has selected an image, want to call ML method
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        // pulling in Vision functionality to analyze image with our ML model
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog! :D"
+                } else {
+                    self.navigationItem.title = "Not hotdog. :("
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
         
     }
     
