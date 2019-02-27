0f11b26a9fa9d452fd6131112ce3f41ab45aa218
bradleypmartin <brad.martin@ibotta.com>
2019-02-26 21:41:01 -0700

    picking out photos to use with ML model (not hooked up yet)

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -11,15 +11,36 @@ import CoreML
 import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
         
+        // may want to set this to true in a more detailed app
+        imagePicker.allowsEditing = false
         
-        
+        // once user has selected an image, want to call delegate method
+
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
